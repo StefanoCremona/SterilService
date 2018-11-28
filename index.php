40fe173ca2e14b00b20332ff7e4275bf2caf404f
stefano.cremona@gmail.com <Babeppe2009>
2018-11-28 21:42:01 +0000

    Add some style to list operation header

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+<form type='post' action='operationList.php'>
 <div>
     <div>Login:</div><div><input type='text'/></div>
 </div>
---
+++ operationList.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+Operations List
+    <div class='listContainer'>
+        <div class='listTitleBar'>
+            <div class='idColumn'>ID</div>
+            <div class='nameColumn'>Name</div>
+            <div class='typeColumn'>Type</div>
+            <div class='dateColumn'>Date</div>
+            <div class='traysColumn'>Trays</div>
+        </div>
+    </div>
+</body>
+</html>
