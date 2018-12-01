481bcab5f1f366d7452b56678642fe28717e020e
stefano.cremona@gmail.com <Babeppe2009>
2018-12-01 19:03:27 +0000

    Update getOperations.php
    Load operations from the database

--- getOperations.php
+++ getOperations.php
@@ -1,12 +1,23 @@
 <?php
 include './models/operation.php';
+include './dbUtils/dbConnection.php';
 
-$arr = array(
-    new Operation('001', 'My Hernia', 'Hernia', '10/10/2018', 5),
-    new Operation('002', 'Your Hernia', 'Hernia', '10/12/2018', 4),
-    new Operation('003', 'Her Hernia', 'Hernia', '10/11/2017', 5),
-    new Operation('004', 'Their Hernia', 'Hernia', '09/09/2018', 5)
-);
+$myDbHelper = new DBHelper();
+$conn = $myDbHelper->getConnection();
+
+$sql = "SELECT id, name, type, dt_creation FROM `procedure`";
+$result = mysqli_query($conn, $sql);
+$arr = array();
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        array_push($arr, new Operation($row["id"], $row["name"], $row["type"], $row["dt_creation"], 5));
+    }
+} else {
+    echo "0 results";
+}
+
+$myDbHelper->closeConnection();
 
 echo json_encode($arr);
 
