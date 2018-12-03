7b07385d62892c644681ab9cc2a54acfd345507a
stefano <stefano.cremona@gmail.com>
2018-12-03 23:50:34 +0000

    Extract Expected trays

--- models/operation.php
+++ models/operation.php
@@ -1,4 +1,8 @@
 <?php
+//These paths depend on the caller path. Be careful.
+include_once './TrayType.php';
+include_once '../dbUtils/dbConnection.php';
+
 class Operation
 {
    function __construct() 
@@ -18,9 +22,27 @@ class Operation
         $this->date = $date;
         $this->trays = $trays;
     }
-    
-    function setTrays() {
-        
+
+    function getExpectedTrays() {
+        $myDbHelper = new DBHelper();
+        $conn = $myDbHelper->getConnection();
+
+        $sql = "SELECT tray_type.ID, tray_type.CODE, tray_type.NAME, TRAY_NUM 
+        FROM `procedure_conf`, `tray_type` WHERE procedure_conf.TRAY_TYPE = tray_type.ID and procedure_type = 1";
+        $result = mysqli_query($conn, $sql);
+        $arr = array();
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+                $tray_type = new TrayType($row["ID"], $row["CODE"], $row["NAME"]);
+                $tray_type->num = $row["TRAY_NUM"];
+                array_push($arr, $tray_type);
+            }
+        }
+
+        $myDbHelper->closeConnection();
+
+        return $arr;
     }
     
     public $id;
