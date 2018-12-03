f40a51dddc0d4710fd81f2526ab99fbdabcbe6a7
stefano.cremona@gmail.com <Babeppe2009>
2018-12-03 00:08:45 +0000

    Create TrayType.php

---
+++ models/TrayType.php
@@ -0,0 +1,35 @@
+<?php
+class TrayType
+{
+    function __construct() 
+    { 
+        $a = func_get_args(); 
+        $i = func_num_args(); 
+        if (method_exists($this,$f='__construct'.$i)) { 
+            call_user_func_array(array($this,$f),$a); 
+        } 
+    }
+    
+    function __construct4($id, $code, $name, $desc)
+    {
+        $this->id = $id;
+        $this->code = $code;
+        $this->name = $name;
+        $this->desc = $desc;
+    }
+    function __construct3($id, $code, $name)
+    {
+        $this->id = $id;
+        $this->code = $code;
+        $this->name = $name;
+    }
+    
+    public $id;
+    public $code;
+    public $name;
+    public $desc;
+    public $dtcreation;
+    public $dtannull;
+
+}
+?>
