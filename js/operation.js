96b7840cee9c9ec0dc21b80f41efebf0cba1d8df
Stefano Cremona <stefano.cremona@gmail.com>
2018-12-08 10:49:12 +0000

    Save tray set operations

--- js/operation.js
+++ js/operation.js
@@ -8,9 +8,39 @@ function hideSpinner() {
 }
 
 function saveInstruments(trayId, instTypeId, instNumber) {
+    if (!instNumber) {
+        alert('Fill the intrument number, Please!');
+        return;    
+    }
+
     showSpinner();
-    console.log(trayId, instTypeId, instNumber);
-    hideSpinner();
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            switch (this.status) {
+                case 200:
+                    try {
+                        var response = JSON.parse(this.responseText);
+                        alert(response.message);
+                        //reload the page
+                        location.reload();
+                    } catch (error) {
+                        alert(error);
+                    }
+                    
+                    break;
+                case 403:
+                case 404:
+                case 500:
+                default:
+                    console.log('Error');
+            }
+            hideSpinner();
+        }
+    }
+    xmlhttp.open("POST", "./json/saveTraySet.php", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("trayId="+trayId+"&instTypeId="+instTypeId+"&instNumber="+instNumber);
 }
 
 function associateTray(operationId, trayTypeId, originalId) {
---
+++ json/saveTraySet.php
@@ -0,0 +1,28 @@
+<?php
+include '../dbUtils/dbConnection.php';
+include_once '../utils/Message.php';
+
+if (!isset($_POST['trayId']) || !isset($_POST['instTypeId']) || !isset($_POST['instNumber'])) {
+    echo json_encode(new Message(false, 'Input not set properly!'));
+    return;
+}
+    
+$trayId = $_POST["trayId"];
+$instTypeId = $_POST["instTypeId"];
+$instNumber = $_POST["instNumber"];
+    
+$myDbHelper = new DBHelper();
+$conn = $myDbHelper->getConnection();
+
+$stmt = mysqli_stmt_init($conn);
+$query = "INSERT INTO `tray_set` (`TRAY_ID`, `INST_TYPE`, `INST_NUM`) VALUES (?, ?, ?)";
+mysqli_stmt_prepare($stmt, $query) or die(json_encode(new Message(false, 'Failed to prepare statement:'.mysqli_stmt_error($stmt))));
+mysqli_stmt_bind_param($stmt, 'iii', $instTypeId, $trayId, $instNumber) or die(json_encode(new Message(false, 'Failed to bind variables:'.mysqli_stmt_error($stmt))));
+mysqli_stmt_execute($stmt) or die(json_encode(new Message(false, 'Failed to execute statement:'.mysqli_stmt_error($stmt))));
+
+echo json_encode(new Message(true, 'Operation successful!'));
+
+$stmt->close();
+$myDbHelper->closeConnection();
+
+?>
